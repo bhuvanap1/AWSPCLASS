cf09e53612c92e23529bd233707b45b9e3add8bc
bhuvanap1 <bhuvanasaikrishna@gmail.com>
2022-02-24 16:18:40 -0500

    Vite

---
+++ vite-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue';
+</script>
+
+<template>
+<Nav />
+
+ <h1 class="title">
+  Hail Valdimer Putin
+</h1>
+<Messages />
+</template>
+
+<style>
+
+</style>
