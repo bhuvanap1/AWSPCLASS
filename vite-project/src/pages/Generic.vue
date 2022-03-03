9c9dd1d31188ca837b1690766fcead25cbfcc52c
bhuvanap1 <bhuvanasaikrishna@gmail.com>
2022-03-03 15:03:59 -0500

    done

---
+++ vite-project/src/pages/Generic.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+const props=defineProps({
+    title:{ type:string,
+    default:"Hello World"}
+})
+
+</script>
+
+<template>
+  
+
+    
+</template>
+
+<style scoped>
+
+</style>
