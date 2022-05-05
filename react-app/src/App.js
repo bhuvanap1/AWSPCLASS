7c26b6d89224f6a120f2c046280144b5807cd4b1
bhuvanap1 <bhuvanasaikrishna@gmail.com>
2022-05-05 15:21:51 -0400

    React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './assets/App.scss';
+import Nav from './components/Nav.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Nav />
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
